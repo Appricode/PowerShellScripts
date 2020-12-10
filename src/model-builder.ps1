b6fbabb74d831524f1bf73ad8e33c0b107cba28b
Can Guldogan <cguldogan@gmail.com>
2020-12-10 10:07:15 +0000

    feat: initial scripts

---
+++ src/model-builder.ps1
@@ -0,0 +1,28 @@
+﻿#This scripts generates C# .cs models by looking to a Yaml files in a directory. 
+#You need to custimize according to your needs
+
+$files =  Get-ChildItem $PSScriptRoot"/yaml" -Filter *.yaml 
+$scriptPath = $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$outSubPath ="\Models\Generated"
+
+foreach ($f in $files){
+    
+$out = Split-Path -Parent -Path $scriptPath
+$inputFile = $f.FullName 
+$namespace = "FileNamePrefix."+ $f.Basename + ".Models" #todo change this according to your code
+$modelFile = $f.Basename + "Models.cs"
+
+Write-Host "Yaml file name : $inputFile"
+Write-Host "Generated Name Space : $namespace"
+
+$ValidateCommand = "swagger-cli validate $inputFile"
+$Command = "nswag openapi2csclient /Namespace:$namespace /Input:$inputFile /GenerateClientClasses:false /GenerateContractsOutput:true /ContractsNamespace:$namespace  /ContractsOutput:$out\$outSubPath\$modelFile /output:$out\$outSubPath\deleteme.cs"
+
+Write-Host "Generated Model File : $modelFile"
+
+Invoke-Expression $ValidateCommand
+Invoke-Expression $Command
+
+Remove-Item "$out\$outSubPath\deleteme.cs"
+Write-Host "Code Generation Competed"
+}
---
+++ src/yaml-downloader.ps1
@@ -0,0 +1,20 @@
+﻿#This is a file downloader. It is not specific to Yaml files. 
+$dictionary = New-Object System.Collections.Generic.Dictionary"[String,String]"
+
+#<fileName,url> list of downloads
+$dictionary.Add("FileName.yaml", "https://fileToDownloadUrl.com")
+
+$scriptPath = $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$outSubPath ="$scriptPath\yaml" #directory and path that files will be saved
+
+foreach ($key in $dictionary.Keys) { 
+    $url = "$($dictionary[$key])"
+    $output = "$outSubPath\$key"
+    $start_time = Get-Date
+
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($url, $output)
+
+    #Start-BitsTransfer -Source $url -Destination $output
+    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+} 
